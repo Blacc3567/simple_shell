675b667c52c8d935393739cc5a78922cdbe29e93
manaye lingerew <113806279+manaye912@users.noreply.github.com>
2022-12-23 01:40:42 +0900

    simple shell

--- _atoi.c
+++ _atoi.c
@@ -1,12 +1,23 @@
 #include "shell.h"
 
+/**
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
 
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
-
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
 int is_delim(char c, char *delim)
 {
 	while (*delim)
@@ -15,7 +26,11 @@ int is_delim(char c, char *delim)
 	return (0);
 }
 
-
+/**
+ *_isalpha - checks for letter
+ *@c: The character to input
+ *Return: 1 if c is lettor, 0 not letter
+ */
 
 int _isalpha(int c)
 {
@@ -25,7 +40,11 @@ int _isalpha(int c)
 		return (0);
 }
 
-
+/**
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, or converted number
+ */
 
 int _atoi(char *s)
 {
